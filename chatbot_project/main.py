ef4fc0d9b2d0cce670ea254a8ae4d427817a9dcd
Alireza KKT <alireza.kh.kkt@gmail.com>
2024-07-08 21:37:34 +0300

    Add Padding token to Tokenizer

--- chatbot_project/main.py
+++ chatbot_project/main.py
@@ -1,13 +1,18 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# بارگذاری مدل و توکنایزر
+# loading model and tokenizer
 model_name = "gpt2"
 model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+#add padding token to tokenizer
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
 def generate_response(prompt):
-    inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(inputs.input_ids, max_length=150, num_return_sequences=1)
+    inputs = tokenizer(prompt, return_tensors="pt" ,padding = True)
+    input_ids = inputs['input_ids']
+    attention_mask = inputs['attention_mask']
+    outputs = model.generate(input_ids, attention_mask = attention_mask, max_length=150, num_return_sequences=1 ,pad_token_id = tokenizer.eos_token_id)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return response
 
