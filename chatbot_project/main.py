ce7d1d32227983f32360757d1c5da6feb4ede778
Alireza KKT <alireza.kh.kkt@gmail.com>
2024-07-08 22:14:36 +0300

    force to Resp in 1 line

--- chatbot_project/main.py
+++ chatbot_project/main.py
@@ -16,18 +16,26 @@ def generate_response(prompt):
 
     outputs = model.generate(input_ids,
      attention_mask = attention_mask, 
-     max_length=100,
+     max_length=50,
+     min_length=10,
       num_return_sequences=1 ,
       pad_token_id = tokenizer.eos_token_id,
-      temperature = 0.7,# handel creativity of chatbot 
-      top_p = 0.9 ,#nucleus sampling(help to repat answers)
+      #temperature = 0.7,# handel creativity of chatbot 
+      #top_p = 0.9 ,#nucleus sampling(help to repat answers)
       top_k=50, # decreas token selection
-      do_sample=True, #for active sampeling
+      do_sample=False, #for active sampeling
       num_beams=5, #use beam search
-      repetition_penalty=2.0, # stop bot for repeting answers
+      repetition_penalty=3.0, # stop bot for repeting answers
       no_repeat_ngram_size=2 , # dont generate duplicate tokens
+      length_penalty=2.0 , # force the model to generate shorter Resp
+
       )
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    stop_sequence='\n'
+    if stop_sequence in response:
+        response = response.split(stop_sequence) [0]
+
     return response
 
 if __name__ == "__main__":
